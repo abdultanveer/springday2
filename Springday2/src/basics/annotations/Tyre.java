003bc3dac792d7a09fcc37fc017932d4f681fcb3
abdul <abdul.tanveer@gmail.com>
2021-04-20 11:49:03 +0530

    constructor injection

--- Springday2/src/basics/annotations/Tyre.java
+++ Springday2/src/basics/annotations/Tyre.java
@@ -3,12 +3,20 @@ package basics.annotations;
 public class Tyre {
 	
 	private String brand;
+	
+	
+
+	public Tyre(String brand) {
+		super();
+		this.brand = brand;
+	}
 
 	public String getBrand() {
 		return brand;
 	}
 
-	public void setBrand(String brand) {
+	public void setBrand(String brand) {      //  <property name="brand" value="MRF"></property>
+
 		this.brand = brand;
 	}
 	
