d1f5e7b7a9824174a22729eb07aa5d23b9299973
abdul <abdul.tanveer@gmail.com>
2021-04-20 11:54:24 +0530

    autowired

--- Springday2/src/basics/annotations/App.java
+++ Springday2/src/basics/annotations/App.java
@@ -7,12 +7,14 @@ public class App {
 	public static void main(String[] args) {
 		//System.out.println("hello world");
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Tyre tyre = (Tyre) context.getBean("tyre");
-		System.out.println(tyre);
-				//new Tyre();
 		/*
-		 * Vehicle vehicle = (Vehicle) context.getBean("bike"); vehicle.drive();
+		 * Tyre tyre = (Tyre) context.getBean("tyre"); System.out.println(tyre);
 		 */
+				//new Tyre();
+		
+		 Vehicle vehicle = (Vehicle) context.getBean("car"); 
+		 vehicle.drive();
+		 
 	}
 
 }
--- Springday2/src/basics/annotations/Car.java
+++ Springday2/src/basics/annotations/Car.java
@@ -1,11 +1,27 @@
 package basics.annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle{
+	
+	@Autowired
+	Tyre tyre;
+	
+	
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
 	public void drive() {
-		System.out.println("driving car");
+		System.out.println("driving car with tyre"+tyre);
 	}
 
 }
