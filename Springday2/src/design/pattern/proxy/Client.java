243e33abe413eadc358fcd7ff9c9ff52a57b47a9
abdul <abdul.tanveer@gmail.com>
2021-04-22 11:27:01 +0530

    proxy design pattern

---
+++ Springday2/src/design/pattern/proxy/Client.java
@@ -0,0 +1,17 @@
+package design.pattern.proxy;
+public class Client
+{
+    public static void main (String[] args)
+    {
+        Internet internet = new ProxyInternet();
+        try
+        {
+            internet.connectTo("geeksforgeeks.org");
+            internet.connectTo("abc.com");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Springday2/src/design/pattern/proxy/Internet.java
@@ -0,0 +1,5 @@
+package design.pattern.proxy;
+public interface Internet
+{
+    public void connectTo(String serverhost) throws Exception;
+}
